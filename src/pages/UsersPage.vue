b6427dfeaa81b9b6dcef189b11e9fd22c26a5ca3
Toxirrrr <mjuraeva74@gmail.com>
2023-03-31 13:53:21 +0500

    new page added

---
+++ src/pages/UsersPage.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <section class="user">
+    <div class="user__wrapp">
+      <h2 class="user__header">
+        Available users
+      </h2>
+
+      <q-list class="user__list">
+        <q-item class="user__item">
+          <q-item-section>
+
+            <span class="user__item-id">
+              1
+            </span>
+
+            <p class="user__item-header">Toxir Turayev</p>
+
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+  </section>
+
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
