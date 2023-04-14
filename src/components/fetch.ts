d9b4248fadcc7ee2be5858a5c58e70c2bae5cc09
ToxirrrrDev <ToxirrrrDev@gmail.com>
2023-04-14 17:26:41 +0500

    login succes

--- src/components/fetch.ts
+++ src/components/fetch.ts
@@ -1,39 +1,47 @@
 export async function postUser(
   user: string,
   pass: string
-): Promise<void | undefined> {
-  const url = 'http://64.227.144.105:3000/api/';
-  const myHeaders = new Headers();
-  myHeaders.append('Content-Type', 'application/json');
+  ): Promise<void | undefined> {
+    const url = 'http://64.227.144.105:3000/api/';
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
 
-  const User = JSON.stringify({
-    username: user,
-    password: pass,
-  });
+    const User = JSON.stringify({
+      login: user,
+      password: pass,
+    });
 
-  await fetch(url + 'auth/sign-in', {
-    method: 'POST',
-    headers: myHeaders,
-    body: User,
-  })
+    await fetch(url + 'auth/sign-in', {
+      method: 'POST',
+      headers: myHeaders,
+      body: User,
+      redirect: 'follow'
+    })
     .then((req) => req.json())
     .then((data) => {
-      const t = data;
-      const tkn = t.token;
+      try {
+        const t = data;
+        const tkn = t.token;
 
-      if (tkn == undefined) {
-        throw new Error('wrong username or password');
+        console.log(t)
+
+        if (tkn == undefined) {
+          throw new Error('wrong username or password');
+        }
+
+        if (!t) {
+          throw new Error(t.error);
+        }
+
+        setTimeout(() => {
+          window.location.href = '/#/order';
+        }, 1000);
+
+        localStorage.setItem('token', t.token);
+      } catch (error) {
+        if(error instanceof Error){
+          throw Error(error.message)
+        }
       }
-
-      if (!t) {
-        throw new Error(t.error);
-      }
-
-
-      setTimeout(() => {
-        window.location.href = '/#/order';
-      }, 1000);
-
-      localStorage.setItem('token', t.token);
     });
-}
+  }
